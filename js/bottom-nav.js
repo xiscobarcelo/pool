2dbc301a0d191b6b353029c387e6b8f514a8763f
xiscobarcelo <xisco.barcelo78@gmail.com>
2025-12-27 21:09:40 +0100

    Update bottom-nav.js

--- js/bottom-nav.js
+++ js/bottom-nav.js
@@ -9,38 +9,28 @@ function initBottomNav() {
             </a>
             
             <a href="estadisticas.html" class="nav-item" data-page="estadisticas">
-                <svg class="nav-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path>
-                </svg>
+                            <img src="images/stats.svg width:24px">
+
                 <span class="nav-label">Estadísticas</span>
             </a>
             
             <a href="torneos.html" class="nav-item" data-page="torneos">
-                <svg class="nav-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"></path>
-                </svg>
+                            <img src="images/cup.svg width:24px">
+
                 <span class="nav-label">Torneos</span>
             </a>
             
             <a href="analisis.html" class="nav-item" data-page="analisis">
-                <svg class="nav-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path>
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path>
-                </svg>
+                            <img src="images/stats1.svg width:24px">
+
                 <span class="nav-label">Análisis</span>
             </a>
-            <a href="historial.html" class="nav-item active">
-    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <path d="M6 9H4.5a2.5 2.5 0 0 1 0-5H6"></path>
-        <path d="M18 9h1.5a2.5 2.5 0 0 0 0-5H18"></path>
-        <path d="M6 9a6 6 0 0 0 12 0"></path>
-        <line x1="12" y1="15" x2="12" y2="17"></line>
-        <line x1="8" y1="17" x2="16" y2="17"></line>
-        <circle cx="18" cy="19" r="3"></circle>
-        <path d="M18 18v1.5l1 .5"></path>
-    </svg>
-    <span>Historial</span>
-</a>
+              <a href="historial.html" class="nav-item" data-page="historial">
+                            <img src="images/historial.svg width:24px">
+
+                <span class="nav-label">Historial</span>
+            </a>
+        
         </div>
     </nav>
     `;
